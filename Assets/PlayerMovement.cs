a945628dd8649213fc263bf32046425fe592ddd5
David Steiner <Dav.d@gmx.de>
2021-11-06 19:47:40 +0100

    add simple demo scene, added player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode runKey = KeyCode.LeftShift;
+
+    [SerializeField]
+    private KeyCode sneakKey = KeyCode.LeftControl;
+
+    [Tooltip("How fast the player is by walking normally")]
+    [SerializeField]
+    private float baseMovementSpeed = 14f;
+
+    [Tooltip("The factor how fast the player is when sneaking compared to walking")]
+    [SerializeField]
+    float sneakMovementFactor = 0.8f;
+
+    [Tooltip("The factor how fast the player is when running compared to walking")]
+    [SerializeField]
+    float runMovementFactor = 1.5f;
+
+    [Tooltip("The lowest input movement amount before it will be ignored")]
+    [SerializeField]
+    float movementInputCutoff = 0.025f;
+
+    [Tooltip("The fraction of the movement input that adds to the velocity")]
+    [SerializeField]
+    float velocityBuildupFraction = 0.1f;
+
+    [Tooltip("The fraction of the velocity that is decreased from time to time")]
+    [SerializeField]
+    float velocityDecreaseFraction = 0.25f;
+
+    [Tooltip("The maximum velocity that can be build up")]
+    [SerializeField]
+    float maxVelocity = 1f;
+
+    [Tooltip("The lowest velocity amount before it will be set to 0")]
+    [SerializeField]
+    float velocityCutoff = 0.005f;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 currentMovement = Vector3.zero;
+    
+    private Rigidbody rigidBody;
+
+    void Awake()
+    {
+        this.rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        ApplyVelocity();
+        ProcessMoveInput();
+        ProcessMovement();
+    }
+
+    private void ApplyVelocity()
+    {
+        velocity *= 1 - velocityDecreaseFraction;
+
+        if (velocity.magnitude < velocityCutoff)
+        {
+            velocity = Vector3.zero;
+            return;
+        }
+    }
+
+    private void ProcessMoveInput()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if(moveDirection.magnitude < movementInputCutoff)
+        {
+            currentMovement = Vector3.zero;
+            return;
+        }
+
+        float movementModifier = 1;
+
+        if (Input.GetKey(sneakKey)){
+            movementModifier = sneakMovementFactor;
+        }
+
+        //running overrides sneaking
+        if (Input.GetKey(runKey))
+        {
+            movementModifier = runMovementFactor;
+        }
+
+        float moveSpeed = baseMovementSpeed * movementModifier;
+        currentMovement = moveDirection * Time.fixedDeltaTime * moveSpeed;
+
+        //add a fraction of the movement to the velocity
+        velocity += velocityBuildupFraction * currentMovement;
+
+        Vector3.ClampMagnitude(velocity, maxVelocity * movementModifier);
+    }
+
+    private void ProcessMovement()
+    {
+        //Sticky wall problem: https://answers.unity.com/questions/585877/rigidbody-sticks-to-walls.html
+
+        rigidBody.AddForce(currentMovement + velocity, ForceMode.VelocityChange);
+    }
+}
+
